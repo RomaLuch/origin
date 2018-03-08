cf62d157c42b681d53e02823c2ec1bf8ed8e5955
RLuchinsky <luchinsky@list.ru>
2018-03-09 01:30:56 +0300

    Crud_Controller_Recactor_1

--- cash/src/main/java/ru/mycash/cash/util/TimeUtil.java
+++ cash/src/main/java/ru/mycash/cash/util/TimeUtil.java
@@ -1,15 +1,13 @@
 package ru.mycash.cash.util;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
-    public static LocalDateTime stringToLocalDateTime(String string) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-        return LocalDateTime.parse(string, formatter);
-    }
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
-        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    public static String toString(LocalDateTime ldt) {
+        return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
 }
