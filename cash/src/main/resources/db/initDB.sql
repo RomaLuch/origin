7aafc33e5dfcd0e9d80a6addb37a6ab526bfd292
RLuchinsky <luchinsky@list.ru>
2018-04-01 21:52:44 +0300

    DB_init_populate_add_categories

--- cash/src/main/resources/db/initDB.sql
+++ cash/src/main/resources/db/initDB.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS records;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS users;
+
+
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -14,12 +18,22 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
+CREATE TABLE categories
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR                 NOT NULL,
+  user_id     INTEGER   NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
 CREATE TABLE records (
   id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id     INTEGER   NOT NULL,
+  category_id     INTEGER   NOT NULL,
   date_time   TIMESTAMP NOT NULL,
   description TEXT      NOT NULL,
   amount    INT       NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
+
 CREATE UNIQUE INDEX record_unique_user_datetime_idx ON records (user_id, date_time);
--- cash/src/main/resources/db/populateDB.sql
+++ cash/src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM records;
+DELETE FROM categories;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -6,13 +7,17 @@ INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
   ('Admin', 'admin@gmail.com', 'admin');
 
+INSERT INTO categories (name, user_id) VALUES
+  ('EAT', 100001),
+  ('ROAD', 100001),
+  ('SHOPING',100001)   ;
 
-INSERT INTO records (date_time, description, amount, user_id) VALUES
-  ('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
-  ('2015-05-30 13:00:00', 'Обед', 1000, 100000),
-  ('2015-05-30 20:00:00', 'Ужин', 500, 100000),
-  ('2015-05-31 10:00:00', 'Завтрак', 500, 100000),
-  ('2015-05-31 13:00:00', 'Обед', 1000, 100000),
-  ('2015-05-31 20:00:00', 'Ужин', 510, 100000),
-  ('2015-06-01 14:00:00', 'Админ ланч', 510, 100001),
-  ('2015-06-01 21:00:00', 'Админ ужин', 1500, 100001);
+INSERT INTO records (date_time, description, amount, user_id, category_id) VALUES
+  ('2015-05-30 10:00:00', 'BIG_MAC', 500, 100000,100002),
+  ('2015-05-30 13:00:00', 'COLA', 1000, 100000, 100002),
+  ('2015-05-30 20:00:00', 'FANTA', 500, 100000, 100002),
+  ('2015-05-31 10:00:00', 'SUSHI', 500, 100000, 100002),
+  ('2015-05-31 13:00:00', 'BUS', 1000, 100000, 100003),
+  ('2015-05-31 20:00:00', 'TRAM', 510, 100000, 100003),
+  ('2015-06-01 14:00:00', 'SHOSE', 510, 100001, 100004),
+  ('2015-06-01 21:00:00', 'phone', 1500, 100001, 100004);
